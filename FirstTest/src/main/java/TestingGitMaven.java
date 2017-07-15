78238f56a8183483f615bb4b73d079d5b78220cd
Sivaraj Velayutham <sivaraj.velayutham88@gmail.com>
2017-07-15 05:24:22 -0400

    First commit

---
+++ FirstTest/src/main/java/TestingGitMaven.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author dell
+ *
+ */
+public class TestingGitMaven {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Testing the build and integration between git and maven for continuous integration");
+
+	}
+
+}
